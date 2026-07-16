ae88b00b3ae650f6f5b154f2a432b760e8b96f07
Eda Atilla <edaatilla1@gmail.com>
2026-07-16 23:42:21 +0300

    colab_calistir.ipynb ve uygulamayi_baslat.bat kaldırıldı

--- uygulamayi_baslat.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo CV Radari baslatiliyor, lutfen bekleyin...
-start "" http://127.0.0.1:7860
-venv\Scripts\python.exe app.py
-echo.
-echo Sunucu durdu. Bu pencereyi kapatabilirsin.
-pause
